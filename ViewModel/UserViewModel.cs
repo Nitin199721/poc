578cebb0c670abd0d6a7a6c6209464c5d11a1527
Nitin199721 <nitin.15bit1095@abes.ac.in>
2019-07-11 19:14:08 +0530

    first commit

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Application.Command;
+using Applications.Model;
+namespace Applications.ViewModel
+{
+    class UserViewModel
+    {
+        private User _user;
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        private ObservableCollection<User> _users;
+        public ObservableCollection<User> users
+        {
+            get { return _users; }
+            set { _users = value; }
+        }
+
+        private ICommand _SubmitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                
+            }
+        }
+    }
+}
